cced36d05ed17bbbda3269c6c90ae72a8f05e739
Aria Nikzad <a.c.nikzad@gmail.com>
2022-10-12 10:33:59 -0700

    added CRUD actions to articles controller, built folders for views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,33 @@
 class ArticlesController < ApplicationController
+
+  def index
+    @articles = Article.all
+    render json: @articles
+  end 
+
+  def show
+    @course = Article.find_by(id: params[:id])
+    render json: @course 
+  end
+
+  def create
+    @article = Article.create(
+      title: params[:title],
+      content: params[:content],
+      author: params[:author],
+      category: params[:category],
+      published_at: params[:published_at]
+      )
+    @article.save
+    render :show
+  end
+
+  def update
+    render :error
+  end
+
+  def destroy
+    render :error
+  end
+
 end
