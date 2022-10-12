abfaaa5e6168a48df4978d557e254dfbf7df8025
Aria Nikzad <a.c.nikzad@gmail.com>
2022-10-12 11:12:15 -0700

    added index.json.vb. Index action working fine.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  namespace :api do
+  # namespace :api do
     get "/articles" => "articles#index"
     get "/articles/:id" => "articles#show"
     post "/articles" => "articles#create"
     patch "/articles/:id" => "articles#update"
     delete "/articles/:id" => "articles#destroy"
-  end
+  # end
 end
